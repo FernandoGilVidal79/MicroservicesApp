021aa53a8e7c6cfc5d0516d991bbb52c005e0a20
fgil <fgil@travelloop.es>
2020-08-23 23:29:44 +0200

    GetProducts Method controller

--- src/Catalog/Catalog.API/Controllers/CatalogController.cs
+++ src/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Catalog.API.Entities;
 using Catalog.API.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API.Controllers
 {
+    [Route("api/v1/[controller]")]
+    [ApiController]
     public class CatalogController : ControllerBase
     {
         private readonly IProductRepository _repository;
@@ -14,6 +18,14 @@ namespace Catalog.API.Controllers
         public CatalogController(IProductRepository repository)
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _repository.GetProducts();
+            return Ok(products);
         }
     }
 }
