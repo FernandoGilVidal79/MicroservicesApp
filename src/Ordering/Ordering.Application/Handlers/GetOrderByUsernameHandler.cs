2c2fe8cd1c53d17aea6953a90ed4884c020c696f
fgil <fgil@travelloop.es>
2021-02-02 23:49:23 +0100

    Ordering using in Handler

--- src/Ordering/Ordering.Application/Handlers/GetOrderByUsernameHandler.cs
+++ src/Ordering/Ordering.Application/Handlers/GetOrderByUsernameHandler.cs
@@ -5,7 +5,6 @@ using Ordering.Application.Responses;
 using Ordering.Core.Repositories;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
