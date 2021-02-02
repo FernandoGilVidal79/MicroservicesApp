014a71b8e3bc80a87ab0c4cf6924995ee5e943f6
fgil <fgil@travelloop.es>
2021-02-02 23:32:02 +0100

    Creation GetOrderByUsernameHandler

---
+++ src/Ordering/Ordering.Application/Handlers/GetOrderByUsernameHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using Ordering.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Handlers
+{
+    public class GetOrderByUsernameHandler : IRequestHandler<GetOrderByUsernameQuery, IEnumerable<OrderResponse>>
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public GetOrderByUsernameHandler(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+        }
+
+
+    }
+}
--- src/Ordering/Ordering.Application/Queries/GetOrderByUsernameQuery.cs
+++ src/Ordering/Ordering.Application/Queries/GetOrderByUsernameQuery.cs
@@ -1,9 +1,19 @@
 ﻿using MediatR;
+using Ordering.Application.Responses;
+using System;
 using System.Collections.Generic;
 
 namespace Ordering.Application.Queries
 {
     public class GetOrderByUsernameQuery : IRequest<IEnumerable<OrderResponse>>
     {
+        public string UserName { get; set; }
+
+        public GetOrderByUsernameQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+
+
     }
 }
