c25871c9ccb5c878fe7f78464caecbdfbc48421e
fgil <fgil@travelloop.es>
2021-02-02 23:25:51 +0100

    Creating orderResponse and nugets references

---
+++ src/Ordering/Ordering.Application/Queries/GetOrderByUsernameQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Queries
+{
+    public class GetOrderByUsernameQuery : IRequest<IEnumerable<OrderResponse>>
+    {
+    }
+}
