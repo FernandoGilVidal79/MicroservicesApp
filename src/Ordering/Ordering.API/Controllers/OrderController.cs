0f198b0ef9ac9f908c38d8a8b75b3b46e3d52951
fgil <fgil@travelloop.es>
2021-02-03 00:10:54 +0100

    Creating OrderController

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Ordering.API.Controllers
 {
@@ -8,5 +14,29 @@ namespace Ordering.API.Controllers
     [Route("[controller]")]
     public class OrderController : ControllerBase 
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUserName(string userName)
+        {
+            var query = new GetOrderByUsernameQuery(userName);
+            var orders = _mediator.Send(query);
+            return Ok(orders);
+        }
+
+        // testing purpose
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckOrder([FromBody] CheckoutOrderCommand command)
+        {      
+            var result = _mediator.Send(command);
+            return Ok(result);
+        }
     }
 }
